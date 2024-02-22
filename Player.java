3b6ca601cf545e74ab17d743d454d34bbdb50e30
sambr <samuel.brown.2022@uni.strath.ac.uk>
2024-02-22 11:42:35 +0000

    NEW FILES!!! YAY

--- Player.java
+++ Player.java
@@ -1,2 +1,44 @@
 public class Player {
+
+    private String username;
+    private int numberOfBulls;
+    private int numberOfCows;
+    private int codesAttempted;
+    private int codesDeciphered;
+
+    public Player(){
+
+    }
+
+    void updateBulls(){
+
+    }
+
+    void updateCows(){
+
+    }
+
+    void incrementCodesAttempted(){
+
+    }
+
+    void incrementCodesDeciphered(){
+
+    }
+
+    void getBulls(){
+
+    }
+
+    void getCows(){
+
+    }
+
+    void getCodesAttempted(){
+
+    }
+
+    void getCodesDeciphered(){
+
+    }
 }
