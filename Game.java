3af8e4add7740bf3736d031bcfb13827e0fb03ff
sambr <samuel.brown.2022@uni.strath.ac.uk>
2024-02-22 14:31:51 +0000

    Added code to enterGuess, submitGuess, and undoGuess. enterGuess and submitGuess are only partially implemented due to lack of information about the rest of the program.

--- Game.java
+++ Game.java
@@ -26,8 +26,57 @@ public class Game {
 
     }
 
-    void enterGuess(){
+    /*
+    Checks if given input position and value are valid for current code-type. If so, inserts it into the
+    guess array at corresponding position
+    @param position the position in the Guess array the value is being entered
+    @param val the value being entered into the Guess array
+    */
+    void enterGuess(int position, char val){
 
+        // Checks if given position is out of range
+        if(position < 0 || position > 4){
+            System.out.println("Invalid position!");
+            return;
+        }
+
+        // If Word Code
+        if(val >= 'a' && val <= 'z') {
+            Guess[position] = val;
+        }
+        else{
+            System.out.println("Must be a letter!");
+        }
+
+        // If Number Code
+        if(val >= '0' && val <= '9'){
+            Guess[position] = val;
+        }
+        else{
+            System.out.println("Must be a number!");
+        }
+
+        return;
+    }
+
+    /*
+    Validates input and passes guess off to be checked
+    @return true if guess matches code
+    */
+    private boolean submitGuess(){
+        if(validateInput() == true){
+            // check if guess matches code
+            // Get number of cows and bulls
+        }
+        return false;
+    }
+
+    /*
+    Sets the value at one of the given places to null
+    @param position number of place to null
+    */
+    private void undoGuess(int position){
+        Guess[position] = '\0';
     }
 
     void saveGuess(){
