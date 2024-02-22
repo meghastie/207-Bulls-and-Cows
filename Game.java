de0325bfe6469288c034a0a6b15b5ab795bd8602
sambr <samuel.brown.2022@uni.strath.ac.uk>
2024-02-22 12:47:27 +0000

    NEW FILES!!! YAY

--- Game.java
+++ Game.java
@@ -1,9 +1,9 @@
 public class Game {
     private Player currentPlayer;
-
+    private char[] Guess;
 
     public Game(Player p, String codeType){
-
+        Guess = new char[4];
     }
 
     public Game(Player p){
@@ -40,5 +40,27 @@ public class Game {
 
     void showSolution(){
 
+    }
+
+    private boolean validateInput(String guess){
+
+        for(int i = 0; i<4; i++){
+
+            // Checks for missing value
+            if(Guess[i] == '\0'){
+                System.out.println("Missing value at position " + i);
+                return false;
+            }
+
+            // Checks for duplicate values
+            for (int j = i; j<4; j++) {
+                if (Guess[i] == Guess[j]) {
+                    System.out.println("Duplicate value present at positions " + i + " & " + j);
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
