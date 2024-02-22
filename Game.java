6589a7ad8533b9a09293a118ff63355dd558ff83
sambr <samuel.brown.2022@uni.strath.ac.uk>
2024-02-22 11:37:33 +0000

    NEW FILES!!! YAY

--- Game.java
+++ Game.java
@@ -1,2 +1,44 @@
 public class Game {
+    private Player currentPlayer;
+
+
+    public Game(Player p, String codeType){
+
+    }
+
+    public Game(Player p){
+
+    }
+
+    void getHint(){
+
+    }
+
+    void loadPlayer(){
+
+    }
+
+    void playGame(){
+
+    }
+
+    void requestCode(){
+
+    }
+
+    void enterGuess(){
+
+    }
+
+    void saveGuess(){
+
+    }
+
+    void loadGame(){
+
+    }
+
+    void showSolution(){
+
+    }
 }
