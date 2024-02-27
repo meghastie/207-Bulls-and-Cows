95979ae3023fd623f0e0de3cdd14a27c2f49ca93
Megan Hastie <megan.hastie.2022@uni.strath.ac.uk>
2024-02-27 20:03:34 +0000

    Basic code to parse the given file of 15 words and then choose a random word from that

--- LettersCode.java
+++ LettersCode.java
@@ -1,9 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class LettersCode extends SecretCode{
 
-    public LettersCode(String file){
+    private List<String> word = new ArrayList<String>(); //holds the 15 options of words from file
+    String gameWord; //the random word chosen for the game
 
+
+    //words from the file are parsed into the arrayList 'word'
+    public LettersCode(String file){
+        gameWord = "NULL";
+        try {
+        Scanner scanner = new Scanner(new File(file));
+        while (scanner.hasNext()) {
+            String line = scanner.next();
+            word.add(line.trim());
+        }
+        scanner.close();
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("File not found");
+        }
     }
 
-    public LettersCode(){}
+    public LettersCode(){
+        gameWord = "NULL";
+    }
 
+    //chooses a random index 0-14. the random index will correspond to one of the 15 words in the file
+    public String chooseWord() {
+
+        if (word.isEmpty()) {
+            System.out.println("Word arraylist is empty - please fill");
+            return "NULL";
+        }
+
+        Random rand = new Random();
+        int randIndex = rand.nextInt(word.size());
+        gameWord = word.get(randIndex);
+
+        return gameWord;
+    }
 }
