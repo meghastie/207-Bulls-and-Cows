ad5dfdfd21b81ca0a5cf0f59f70af0824f7bf143
sambr <samuel.brown.2022@uni.strath.ac.uk>
2024-03-07 14:12:37 +0000

    Just changed all fields to private, and fixed things where needed

--- BullsAndCows/Players.java
+++ BullsAndCows/Players.java
@@ -21,19 +21,55 @@ public class Players {
 
     }
 
-    void getAllPlayerBulls(){
+    /*
+    Sums the total number of Bulls achieved by all stored players
 
+    @returns The total number of Bulls
+     */
+    int getAllPlayerBulls(){
+        int totalBulls = 0;
+        for(Player p : allPlayers){
+            totalBulls = totalBulls + p.getBulls();
+        }
+        return totalBulls;
     }
 
-    void getAllPlayerCows(){
+    /*
+    Sums the total number of Cows achieved by all stored players
 
+    @returns The total number of Cows
+     */
+    int getAllPlayerCows(){
+        int totalCows = 0;
+        for(Player p : allPlayers){
+            totalCows = totalCows + p.getCows();
+        }
+        return totalCows;
     }
 
-    void getAllPlayersSecretCodesAttempted(){
+    /*
+    Sums the total number of Secret Codes attempted by all stored players
 
+    @returns The total number of Secret Codes attempted
+     */
+    int getAllPlayersSecretCodesAttempted(){
+        int totalCodes = 0;
+        for(Player p : allPlayers){
+            totalCodes = totalCodes + p.getCodesAttempted();
+        }
+        return totalCodes;
     }
 
-    void getAllPlayersSecretCodesDeciphered(){
+    /*
+    Sums the total number of Secret Codes deciphered by all stored players
 
+    @returns The total number of Secret Codes deciphered
+     */
+    int getAllPlayersSecretCodesDeciphered(){
+        int totalCodes = 0;
+        for(Player p : allPlayers){
+            totalCodes = totalCodes + p.getCodesDeciphered();
+        }
+        return totalCodes;
     }
 }
